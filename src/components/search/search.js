44ba7d8412db0e58065d3682a4415279fb2f767f
SongChan-Gyu <zx2253@naver.com>
2019-03-30 18:33:59 +0900

    first

---
+++ src/components/search/search.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import {Button} from 'semantic-ui-react';
+
+const search = () => {
+    return(
+        <Button primary>강의실 찾기</Button>
+    )
+}
+
+export default search;
